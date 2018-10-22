5314ae8f8e183b21bbade35ae15e92abc791faa1
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2018-10-22 02:52:16 -0500

    redistricting project

--- Code/stl/tuple.cxx
+++ Code/stl/tuple.cxx
@@ -2,7 +2,7 @@
  ****
  **** This file belongs with the course
  **** Introduction to Scientific Programming in C++/Fortran2003
- **** copyright 2016/7 Victor Eijkhout eijkhout@tacc.utexas.edu
+ **** copyright 2018 Victor Eijkhout eijkhout@tacc.utexas.edu
  ****
  **** tuple.cxx : std::tuple
  ****
@@ -14,9 +14,50 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+#include <cmath>
+
+#include <tuple>
+using std::make_tuple;
 using std::tuple;
 
+//codesnippet tuplemake
+auto maybe_root1(float x) {
+  if (x<0)
+    return make_tuple<bool,float>(false,-1);
+  else
+    return make_tuple<bool,float>(true,sqrt(x));
+};
+//codesnippet end
+
+//codesnippet tupledenote
+tuple<bool,float> maybe_root2(float x) {
+  if (x<0)
+    return {false,-1};
+  else
+    return {true,sqrt(x)};
+};
+//codesnippet end
+
 int main() {
+  float x;
+  cin >> x;
+  {
+    //codesnippet tupleauto
+    auto [succeed,y] = maybe_root1(x);
+    if (succeed)
+      cout << "Root of " << x << " is " << y << endl;
+    else
+      cout << "Sorry, " << x << " is negative" << endl;
+    //codesnippet tupleauto
+  }
+  
+  if (false) {
+    auto [succeed,y] = maybe_root2(x);
+    if (succeed)
+      cout << "Root of " << x << " is " << y << endl;
+    else
+      cout << "Sorry, " << x << " is negative" << endl;
+  }
   return 0;
 }
 
