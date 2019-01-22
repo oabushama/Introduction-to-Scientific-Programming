f0f0b541c3266a7b23131c5772dbdac70a084f51
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2019-01-22 13:55:08 -0600

    updating

--- Code/rsa/rsa.cxx
+++ Code/rsa/rsa.cxx
@@ -5,6 +5,7 @@
  **** copyright 2018/9 Victor Eijkhout eijkhout@tacc.utexas.edu
  ****
  **** rsa.cxx : simulation of RSA encryption
+ **** following https://www.geeksforgeeks.org/rsa-algorithm-cryptography/
  ****
  ****************************************************************/
 
@@ -22,6 +23,17 @@ using std::vector;
 #include <limits>
 using std::numeric_limits;
 
+long long gcd(long long i,long long j) {
+  if (i>j)
+    return gcd(j,i);
+  else {
+    if (j%i==0)
+      return i;
+    else
+      return gcd(j%i,i);
+  }
+};
+
 bool test_if_prime(long long number) {
   for (long long divisor=2; divisor<number; divisor++) {
     if (number%divisor==0) {
@@ -38,20 +50,50 @@ long long next_prime(long long start) {
   return start;
 };
 
-auto encrypt(string msg,long long n,long long d) {
-  vector<long long> encrypted;
+long long phi_n(long long p,long long q) {
+  return (p-1)*(q-1);
+};
+
+long long e_under_phi(long long n,long long phi) {
+  long long e=3;
+  while ( gcd(n,e)!=1 )
+    e++;
+  if (e>=phi) {
+    cout << "Could not find e under phi" << endl;
+    throw(1);
+  }
+  return e;
+};
+
+auto crypt(vector<long long> msg,long long n,long long e) {
+  vector<long long> crypted;
   for ( auto c : msg ) {
     long long nd  = 1;
-    for ( int id=0; id<d; id++) {
+    for ( int id=0; id<e; id++) {
       if (nd>numeric_limits<long long>::max()/c) {
-	cout << "encryption overflows at power " << id << endl;
+	cout << "cryption overflows at power " << id << endl;
 	throw(1);
       }
       nd = (nd%n) * c ; // nd *= c;
     }
-    encrypted.push_back( nd%n );
+    crypted.push_back( nd%n );
   }
-  return encrypted;
+  return crypted;
+};
+
+auto encrypt(string msg, long long n,long long e) {
+  vector<long long> msgnums;
+  for ( auto c : msg )
+    msgnums.push_back(c);
+  return crypt(msgnums,n,e);
+};
+
+auto decrypt(vector<long long> cmsg,long long n,long long d) {
+  string dmsg;
+  for ( auto c : crypt(cmsg,n,d) ) {
+    dmsg.push_back(c);
+  };
+  return dmsg;
 };
 
 int main() {
@@ -64,25 +106,32 @@ int main() {
   cout << "Found factors " << factor1 << "," << factor2 << endl;
   long long bignum = factor1*factor2;
   cout << "  with product=" << bignum << endl;
-  long long d = next_prime(factor2+1);
-  if (d>=bignum) {
-    cout << "Could not find d" << endl; return 1;
-  }
+
+  long long phi = phi_n(factor1,factor2);
+  long long e = e_under_phi(bignum,phi);
+
+  int k=2;
+  long long d = (k*phi+1)/e;
   cout << "  and d=" << d << endl;
   
-  vector<long long> crypt;
+  vector<long long> encrypted;
   try {
-    crypt = encrypt("message",bignum,d);
+    encrypted = encrypt("message",bignum,e);
   } catch (...) {
-    cout << "encryption failed" << endl;
+    cout << "cryption failed" << endl;
     return 1;
   }
 
   cout << "encrypted:";
-  for ( auto c : crypt ) cout << " " << c;
+  for ( auto c : encrypted ) cout << " " << c;
   cout << endl;
 
   // the key is bignum,d so we can use that
+  auto decrypted = decrypt(encrypted,bignum,d);
   
+  cout << "decrypted:";
+  for ( auto c : decrypted ) cout << " " << c;
+  cout << endl;
+
   return 0;
 }
